6247ec4f3c30c6d7c0994235346caaa8d966c7d2
Melvin <chen.melv@husky.neu.edu>
2019-01-30 21:53:06 -0500

    initial commit

---
+++ js/randomizing.js
@@ -0,0 +1,17 @@
+function randomize() {
+    var inputString = $('#phrase').val();
+    var inputIntoArray = inputString.split("");
+    var finalString = "";
+
+    inputIntoArray.forEach(function(element) {
+        if (Math.random() <= .5) {
+            finalString += element.toString().toUpperCase();
+        }
+        else {
+            finalString += element.toString();
+        }
+        console.log("f: " + element);
+    });
+    
+   console.log(finalString);
+}
