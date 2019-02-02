10a700cfdffd7d8ced5224c9e8e73dd3397f94c5
Melvin <chen.melv@husky.neu.edu>
2019-02-01 21:24:03 -0500

    style-ized

--- js/randomizing.js
+++ js/randomizing.js
@@ -12,7 +12,23 @@ function randomize() {
         }
         //console.log("f: " + element);
     });
-    console.log($('#place').text());
+    //console.log($('#place').text());
     $('#place').empty();
     $('#place').append(finalString);
 }
+
+function copy(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+
+$('#phrase').keypress(function(event) {
+    if (event.keyCode == 13 || event.which == 13) {
+        //$('#submitButton').focus();
+        randomize();
+        event.preventDefault();
+    }
+});
