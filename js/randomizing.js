de8c1aede7f9bc8b537f9b86bdf7b9e0a744f79f
Melvin <chen.melv@husky.neu.edu>
2019-02-09 11:02:01 -0500

    fix randomization

--- js/randomizing.js
+++ js/randomizing.js
@@ -2,19 +2,50 @@ function randomize() {
     var inputString = $('#phrase').val();
     var inputIntoArray = inputString.split("");
     var finalString = "";
+    var upperCounter = 0;
+    var lowerCounter = 0;
+    var flag = false; //true = upper, false = lower
+    //abcdefghijk
+    //aaaaaaaaaaa
 
     inputIntoArray.forEach(function(element) {
+        // console.log("UC: " + upperCounter + " LC: " + lowerCounter + " ele: " + element);
         if (Math.random() <= .5) {
-            finalString += element.toString().toUpperCase();
+            //there are fewer than three upper case letters in a row
+            if (upperCounter <= 2) {
+                lowerCounter = 0;
+                finalString += element.toString().toUpperCase();
+                upperCounter++;
+            }
+            //there are more than three upper case letters in a row
+            else {
+                finalString += element.toString().toLowerCase();
+                upperCounter = 0;
+                lowerCounter++;
+            }
         }
         else {
-            finalString += element.toString();
+            if (lowerCounter <= 2) {
+                upperCounter = 0;
+                finalString += element.toString().toLowerCase();
+                lowerCounter++;
+            }
+            else {
+                finalString += element.toString().toUpperCase();
+                lowerCounter = 0;
+                upperCounter++;
+            }
         }
         //console.log("f: " + element);
     });
     //console.log($('#place').text());
+    // $('#place').empty();
+    // finalString.split("").forEach(function(element) {
+    //     $('#place').append(element);
+    // })
     $('#place').empty();
     $('#place').append(finalString);
+    console.log(finalString);
 }
 
 function copy(element) {
