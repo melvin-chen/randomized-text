760cf06bed360fef76bc21ebd73f8cbd2cdbe10d
Melvin <chen.melv@husky.neu.edu>
2019-02-09 16:38:17 -0500

    speed fixing

--- js/randomizing.js
+++ js/randomizing.js
@@ -1,4 +1,5 @@
-function randomize() {
+/* function randomize() {
+    var t0 = performance.now();
     var inputString = $('#phrase').val();
     var inputIntoArray = inputString.split("");
     var finalString = "";
@@ -6,6 +7,7 @@ function randomize() {
     var lowerCounter = 0;
     //abcdefghijk
     //aaaaaaaaaaa
+    console.log(inputIntoArray[0]);
 
     inputIntoArray.forEach(function(element) {
         if (Math.random() <= .5) {
@@ -36,6 +38,34 @@ function randomize() {
     $('#place').empty();
     $('#place').append(finalString);
     console.log(finalString);
+    var t1 = performance.now();
+    console.log("Call to randomize took " + (t1 - t0) + " milliseconds.")
+} */
+
+function randomize2() {
+    var t0 = performance.now();
+    var inputString = $('#phrase').val().toLowerCase();
+    var inputIntoArray = inputString.split("");
+    // console.log(inputString);
+    var len = inputString.length;
+    //console.log(inputString.length);
+    var capitalIndex = 1;
+
+    while (capitalIndex < (inputString.length - 1)) {
+        // console.log(inputString.charAt(capitalIndex).toUpperCase());
+        inputIntoArray[capitalIndex] = inputString.charAt(capitalIndex).toUpperCase();
+        if (Math.random() <= .5) {
+            capitalIndex += 2;
+        } else {
+            capitalIndex += 3;
+        }
+    }
+    var finalString = inputIntoArray.join("");
+    $('#place').empty();
+    $('#place').append(finalString);
+    var t1 = performance.now();
+    console.log(inputIntoArray.join(""));
+    console.log("Call to randomize2 took " + (t1 - t0) + " milliseconds.")
 }
 
 function copy(element) {
@@ -44,12 +74,12 @@ function copy(element) {
     $temp.val($(element).text()).select();
     document.execCommand("copy");
     $temp.remove();
-  }
+}
 
-$('#phrase').keypress(function(event) {
+$('#phrase').keypress(function (event) {
     if (event.keyCode == 13 || event.which == 13) {
         //$('#submitButton').focus();
-        randomize();
+        randomize2();
         event.preventDefault();
     }
 });
