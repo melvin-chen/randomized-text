c840a7fe9074c7f24c685e0610828490a21e81df
Melvin Chen <mchen1626@gmail.com>
2020-08-16 21:59:55 -0400

    enter key

--- js/randomizing.js
+++ js/randomizing.js
@@ -43,43 +43,45 @@
 } */
 
 function randomize2() {
-    var t0 = performance.now();
-    var inputString = $('#phrase').val().toLowerCase();
-    var inputIntoArray = inputString.split("");
-    // console.log(inputString);
-    var len = inputString.length;
-    //console.log(inputString.length);
-    var capitalIndex = 1;
+  var inputString = $("#phrase").val().toLowerCase();
+  var inputIntoArray = inputString.split("");
+  var capitalIndex = 1;
 
-    while (capitalIndex < (inputString.length - 1)) {
-        // console.log(inputString.charAt(capitalIndex).toUpperCase());
-        inputIntoArray[capitalIndex] = inputString.charAt(capitalIndex).toUpperCase();
-        if (Math.random() <= .5) {
-            capitalIndex += 2;
-        } else {
-            capitalIndex += 3;
-        }
+  while (capitalIndex < inputString.length - 1) {
+    inputIntoArray[capitalIndex] = inputString
+      .charAt(capitalIndex)
+      .toUpperCase();
+    if (Math.random() <= 0.5) {
+      capitalIndex += 2;
+    } else {
+      capitalIndex += 3;
     }
-    var finalString = inputIntoArray.join("");
-    $('#place').empty();
-    $('#place').append(finalString);
-    var t1 = performance.now();
-    console.log(inputIntoArray.join(""));
-    console.log("Call to randomize2 took " + (t1 - t0) + " milliseconds.")
+  }
+  var finalString = inputIntoArray.join("");
+  $("#place").empty();
+  $("#place").append(finalString);
 }
 
 function copy(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
+  var $temp = $("<input>");
+  let $successText = $("#successText");
+
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+
+  $successText.css('opacity', '1')
+  setTimeout(() => {
+    $successText.css('opacity', '0')
+  }, 800);
 }
 
-$('#phrase').keypress(function (event) {
-    if (event.keyCode == 13 || event.which == 13) {
-        //$('#submitButton').focus();
-        randomize2();
-        event.preventDefault();
-    }
+$("#phrase").keypress(function (event) {
+  if (event.keyCode == 13 || event.which == 13) {
+    randomize2();
+    copy("#place");
+    document.querySelector('#phrase').value = '';
+    event.preventDefault();
+  }
 });
